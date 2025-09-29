f3a04c6e2e43f80471e85e18de87900603c00205
Sakshamebdev873 <vinodarya344@gmail.com>
2025-09-29 10:40:55 +0530

    added the embedContent logic

---
+++ dist/libs/embedProduct.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=embedProduct.d.ts.map
---
+++ libs/embedProduct.ts
@@ -0,0 +1,29 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import pool from "../database/db.js";
+ 
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
+const model = genAI.getGenerativeModel({ model: "gemini-embedding-001" });
+async function embedProducts(){
+    const products = await pool.query(
+        `SELCT id,nmae,description
+        FROM products
+        WHERE id NOT IN (SELECT product_id FROM product_embeddings)`
+    )
+    for(const p of products.rows){
+        const text = `${p.name}. ${p.description ?? ""}`
+        try {
+            const result=  await model.embedContent(text)
+            const embedding= result.embedding.values
+            await pool.query(`INSERT INTO product_embeddings (product_id, embedding) VALUES ($1,$2)`),[p.id,embedding]
+        console.log(`✅ Embedded product ${p.id} - ${p.name}`);
+        } catch (err
+            :any
+        ) {
+             console.error(`❌ Failed embedding for product ${p.id}:`, err);
+        }
+    }
+}
+embedProducts().then(() => {
+  console.log("🎉 Embedding generation complete");
+  process.exit(0);
+});
